f560285a2daf38d358bca8391ff9a62477541dde
Ivan B <79531889+beep-boop-boop@users.noreply.github.com>
2021-12-02 14:03:47 +0000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,14 @@
+with open('inputs/input_day_1.txt', 'r') as file:
+    numbers = file.readlines()
+    numbers = [int(number.strip()) for number in numbers]
+    groups = []
+    count = 0
+    for i in range(len(numbers) - 2):
+        groups.append([])
+        for d in range(i, i + 3):
+            groups[i].append(numbers[d])
+    groups = [sum(group) for group in groups]
+    for i in range(len(groups) - 1):
+        if groups[i + 1] > groups[i]:
+            count += 1
+print(count)
